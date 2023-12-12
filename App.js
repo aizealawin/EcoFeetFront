773172f83e2d2731bb17ec07386be30a7b9956ed
aizealawin <ktmudalige@gmail.com>
2023-12-12 12:23:00 -0500

    imports in app.js

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
+import * as React from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as WebBrowser from "expo-web-browser"
+import * as Google from 'expo-auth-session/providers/google'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+WebBrowser.maybeCompleteAuthSession();
+
 
 export default function App() {
+const [userInfo, setUserInfo] = React.useState(null);
+
+
+
   return (
     <View style={styles.container}>
       <Text>EcoFeat!</Text>
